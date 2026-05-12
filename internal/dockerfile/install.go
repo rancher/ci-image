e6891fd40c347ab7f04434b98238069d62f652d4
matttrach <matt.trachier@suse.com>
2026-05-12 16:48:08 -0500

    fix: remove is script url function
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- internal/dockerfile/install.go
+++ internal/dockerfile/install.go
@@ -46,8 +46,3 @@ func isGzipBinaryURL(url string) bool {
 	return strings.HasSuffix(u, ".gz") && !strings.HasSuffix(u, ".tar.gz")
 }
 
-// isScriptURL reports whether url is an installation script.
-func isScriptURL(url string) bool {
-	u := urlExt(url)
-	return strings.HasSuffix(u, ".sh") || strings.HasSuffix(u, "/install")
-}
